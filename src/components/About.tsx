1e14140c306a2b64ad290cd1dde23247d88613ac
Piyush Paliwal <ppaliwal868@gmail.com>
2024-10-17 01:57:34 +0530

    name changed

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -93,7 +93,7 @@ const About = () => {
                     size="sm"
                     color="#64ffda"
                   >
-                    DOWNLOAD
+                    DOWNLOAD RESUME
                   </Button>
                 </a>
               </div>
